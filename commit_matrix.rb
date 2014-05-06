6d158f931dd5e927dc80f8087c05dd51d44db44b
Jack Bracewell <jbtwentythree@gmail.com>
2014-05-06 19:29:58 +0100

    Fully working, storing only non-nil values.

--- commit_matrix.rb
+++ commit_matrix.rb
@@ -45,14 +45,12 @@ class CommitMatrix
 
   def create_new_file_with_history filename
     @filenames_to_columns[filename] = @number_of_files
-    #@columns[@number_of_files] = Array.new(rows.size).map(&:to_i)
-    @columns[@number_of_files] = []
     @number_of_files += 1
   end
 
   def delete_file filename
+    #Todo, archive
     column = @filenames_to_columns.delete filename
-    #columns.delete column
   end
 
   def next_commit
@@ -90,18 +88,22 @@ class CommitMatrix
     matrix2 = Marshal.load(Marshal.dump(matrix2))
 
     most_recent_ancestor = `git merge-base #{[matrix1,matrix2].map{ |m| m.commit_hash }.join " "}`.chomp
-    slice_index = matrix2.rows.index(most_recent_ancestor) + 1
 
     size_of_matrix1_diff = `git rev-list #{most_recent_ancestor}..#{matrix1.commit_hash} --count`.to_i
 
+    new_commits = matrix2.rows.select { |k,v| !matrix1.rows.keys.include? k }.keys
+    matrix1.rows = matrix2.rows.merge matrix1.rows
+    #for renames and additions we need to go through the whole set of 'new' commits and convert the old column values
+    #to the new ones.
+    renamed_files = []
     diff.split("\n").each do |file|
       words = file.split
       if words.first =~ /R.*/
         matrix1.rename_file words[-2], words[-1]
-        size = matrix1.columns[matrix1.filenames_to_columns[words[-1]]].size
-        matrix1.columns[matrix1.filenames_to_columns[words[-1]]].delete_at size-1
+        #Not sure about this..
       elsif words.first =~ /A.*/
         matrix1.create_new_file_with_history words.last
+        renamed_files << words.last
       elsif words.first =~ /M.*/
         #Don't need to do anything
       elsif words.first =~ /D.*/
@@ -109,59 +111,26 @@ class CommitMatrix
       end
     end
 
-    mash_lists matrix1, matrix2, :end_at => most_recent_ancestor
-
-    #Dollop rows from matrix2 ontop of matrix1
-    puts 'slow'
-    matrix1.filenames_to_columns.each do |k,v|
-      if matrix2.filenames_to_columns.has_key? k
-        new_values = matrix2.columns[matrix2.filenames_to_columns[k]].select do |k,v|
-          matrix1.rows.index { |hash| hash == k }
+    new_commits.each do |hash|
+      new_list = []
+      renamed_files.each do |filename|
+        col = matrix1.filenames_to_columns[filename]
+        old_col = matrix2.filenames_to_columns[filename]
+        list = matrix1.rows[hash]
+        if list.include? old_col
+          list.delete old_col
+          new_list << col
         end
-        matrix1.columns[v].concat new_values
-      else
-        #matrix1.columns[v].concat Array.new(matrix2.number_of_commits-slice_index).map(&:to_i)
+        matrix1.rows[hash] = list + new_list
       end
     end
-    matrix1.rows += matrix2.rows[slice_index..-1]
+
     matrix1.number_of_commits = matrix1.rows.size
-    matrix1.rows << commit_hash
+    matrix1.rows[commit_hash] = []
     matrix1.commit_hash = commit_hash
     matrix1.next_commit
     matrix1
   end
 
-  #precondition, the lists will have the same base and the same end
-  #Essentially, take everything in matrix2 that is not in matrix1, and
-  #shove it in. (before the specified point -- the most recent common
-  #ancestor)
-  def self.mash_lists matrix1, matrix2, opts={}
-    return if opts[:end_at] == matrix1.rows.first
-    i1 = 0
-    i2 = 0
-    list1 = matrix1.rows
-    list2 = matrix2.rows
-
-    list2.each do |item|
-      while (item != list1[i1]) do
-        if list1.include? item
-          i1 = list1.index item
-        else
-          list1.insert i1, item
-          matrix1.filenames_to_columns.each do |k,v|
-            if !matrix2.filenames_to_columns[k]
-              matrix1.columns[v].insert i1, 0
-            else
-              matrix1.columns[v].insert i1, matrix2.columns[matrix2.filenames_to_columns[k]][i2]
-            end
-          end
-        end
-      end
-      i1 += 1
-      i2 += 1
-      return list1 if opts[:end_at] && opts[:end_at] == item
-    end
-    list1
-  end
 
 end
