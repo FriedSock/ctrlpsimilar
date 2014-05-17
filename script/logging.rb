712bfa4fe449901681d7833c77560e378968b776
Jack Bracewell <jbtwentythree@gmail.com>
2014-05-17 12:29:28 +0100

    Fix logging so it is actually JSON parsable

--- script/logging.rb
+++ script/logging.rb
@@ -6,5 +6,5 @@ def log_action str
   index = sim_files.index { |item| item.split.first.chomp == str.chomp }
   similarity = sim_files[index].split.last
   size = sim_files.size
-  puts `echo '{ filename:"#{str}", index: #{index}, size: #{size}, similarity: #{similarity}, time:"#{Time.new}"},' >> #{LOG_PATH}`
+  puts `echo '{ "filename":"#{str}", "index": #{index}, "size": #{size}, "similarity": #{similarity}, "time":"#{Time.new}"},' >> #{LOG_PATH}`
 end
